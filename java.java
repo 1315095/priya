18358c31eb40fe2930cf25cfba2bd8842a46c59a
1315095 <priyankaboopathi.ece@gmail.com>
2016-08-20 14:10:44 +0530

    Add files via upload

---
+++ java.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class java {
+	public static void main(String[] arg) 
+	{ Scanner in=new Scanner(System.in); 
+	int n=in.nextInt(); 
+	if(n==0) 
+		System.out.println("n is zero");
+	else if(n>0)
+		System.out.println("n is positive");
+		else System.out.println("n is negative"); }
+
+}
