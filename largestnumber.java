b0c48b17e66203f806fc5f995a8c3c166196cc5e
1315095 <priyankaboopathi.ece@gmail.com>
2016-08-20 15:59:23 +0530

    Create largestnumber.java

---
+++ largestnumber.java
@@ -0,0 +1,14 @@
+public static void main(String[] arg)
+{
+Scanner in=new Scanner(System.in);
+int a,b,c;
+a=in.nextInt();
+b=in.nextInt();
+c=in.nextInt();
+if((a>b)&&(a>c))
+System.out.println(a+" is greater");
+else if(b>c)
+System.out.println(b+" is greater");
+else
+System.out.println(c+" is greater");
+}
