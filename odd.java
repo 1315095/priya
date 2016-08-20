72233fc2bfaf7d0064546b12c8fb294630f7eb6b
1315095 <priyankaboopathi.ece@gmail.com>
2016-08-20 15:05:20 +0530

    Update and rename priya.java to odd.java

---
+++ odd.java
@@ -0,0 +1,15 @@
+# priya
+import java.util.Scanner;
+public class even {
+	public static void main(String[] arg) 
+	{ Scanner in=new Scanner(System.in); 
+	System.our.println("enter the number");
+	int n=in.nextInt(); 
+	if(n%2==0) 
+		System.out.println("n is even");
+	else 
+		System.out.println("n is odd");
+		     }
+
+}
+
