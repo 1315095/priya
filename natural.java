54a3c2518675af6e85a7ac6b79bf99dee8721eee
1315095 <priyankaboopathi.ece@gmail.com>
2016-08-20 17:31:54 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class natural{
+
+    
+ public static void main(String []args)
+{
+ 
+
+Scanner in=new Scanner(System.in);
+
+int n,sum;
+
+System.out.print("enter the value of n:");
+
+n=in.nextInt();
+
+
+ 
+sum=n*(n+1)/2;
+
+System.out.println(sum);
+
+
+
+}
+
+}
